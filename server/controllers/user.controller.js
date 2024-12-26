5b09f0da2070e906bf55689d42553ff3d808b79c
Sourav <souravvaishyakiyar@gmail.com>
2024-12-26 17:59:11 +0530

    Creating routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model.js";
+import {User} from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateToken } from "../utils/generateToken.js";
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import connectDB from './database/db.js'; // Import the connectDB function
+import userRoute from './routes/user.route.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 
 const app = express();
 const PORT = 8080;
 
+dotenv.config();
+
 // Connect to MongoDB
 connectDB();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:8080"],
+    credentials: true,
+}));
+
+app.use("/api/v1/user",userRoute);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
