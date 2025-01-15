a30a624e191679201ad4a5176f6b84a5480e11e2
Sourav <souravvaishyakiyar@gmail.com>
2025-01-15 17:30:14 +0530

    Build Course Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import { Route } from 'react-router'
 import { RouterProvider } from 'react-router'
+import Courses from './pages/student/Courses'
+import Course from './pages/student/Course'
 
 const appRouter=createBrowserRouter([
   {
@@ -20,6 +22,8 @@ const appRouter=createBrowserRouter([
         element:(
           <>
           <HeroSection/>
+          <Courses/>
+         
           </>
         ),
         
---
+++ client/src/pages/student/Course.jsx
@@ -0,0 +1,43 @@
+import { AvatarFallback } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent } from '@/components/ui/card'
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
+
+import React from 'react'
+
+
+const Course = () => {
+  return (
+    <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
+      <div className="relative">
+        <img
+          src="https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png"
+          alt="Course Image"
+         className="w-full h-36 object-cover rounded-t-lg"
+        />
+      </div>
+        <CardContent className="px-5 py-4 space-y-3">
+      <h1 className="hover:underline font-bold text-lg truncate">Next Js Complete Course
+
+      </h1>
+      <div className='flex items-center justify-between'>
+      <div className="flex items-center gap-3">
+      <Avatar className="h-8 w-8">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <h1 className="font-medium text-sm">Instructor</h1>
+        
+      </div>
+      <Badge className={'bg-blue-600 text-white px-2 py-1 text-xs rounded-full'}>Advance</Badge>
+      </div>
+      <div className="text-lg font-bold">
+        <span>₹ 499</span>
+      </div>
+      
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Course
