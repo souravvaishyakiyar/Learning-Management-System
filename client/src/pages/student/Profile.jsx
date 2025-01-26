e54674d1220ddcc67ffde629f622fb8646ff3e46
Sourav <souravvaishyakiyar@gmail.com>
2025-01-26 15:19:01 +0530

    Minor changes in Profile pages

--- client/src/pages/student/Profile.jsx
+++ client/src/pages/student/Profile.jsx
@@ -21,14 +21,20 @@ const Profile = () => {
   const {data, isLoading} = useLoadUserQuery();
 
   console.log(data);
-  const enrolledCourses = [];
+ 
+
+  if(isLoading){
+   return  <h1>Profile is loading...</h1>
+  }
+
+  const {user}=data;
   return (
     <div className="max-w-4xl mx-auto my-24 px-4 md:px-0">
       <h1 className="font-bold text-2xl text-center md:text-left">PROFILE</h1>
       <div className="flex flex-col md:flex-row items-center md:items-start gap-8 my-5">
         <div className="flex flex-col items-center">
           <Avatar className="h-24 w-24 md:h-32 md:w-32 mb-4">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarImage src={user.photoUrl || "https://github.com/shadcn.png"} alt="@shadcn" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div>
@@ -36,7 +42,7 @@ const Profile = () => {
               <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
                 Name:
                 <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-                  John
+                  {user.name}
                 </span>
               </h1>
             </div>
@@ -44,7 +50,7 @@ const Profile = () => {
               <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
                 Email:
                 <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-                  John@gmail.com
+                  {user.email}
                 </span>
               </h1>
             </div>
@@ -52,7 +58,7 @@ const Profile = () => {
               <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
                 Role:
                 <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-                  Instructor
+                  {user.role.toUpperCase()}
                 </span>
               </h1>
             </div>
@@ -121,10 +127,10 @@ const Profile = () => {
         <div>
           <h1 className="font-medium text-lg">Courses you are enrolled in</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5">
-            {enrolledCourses.length === 0 ? (
+            {user.enrolledCourses.length === 0 ? (
               <h1>You have not  enrolled yet</h1>
             ) : (
-              enrolledCourses.map((course) => (
+              user.enrolledCourses.map((course) => (
                 <Course course={course} key={course._id} />
               ))
             )}
